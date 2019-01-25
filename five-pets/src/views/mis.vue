0c82b70f16576c2e51d2f42aac7bb58676b8c4f6
月夜小白龙 <594877445@qq.com>
2019-01-25 11:06:00 +0800

    修改导航栏

--- five-pets/src/views/mis.vue
+++ five-pets/src/views/mis.vue
@@ -8,30 +8,72 @@
     >
       <el-submenu index="1">
         <template slot="title">
-          <i class="el-icon-location"></i>
+          <i class="el-icon-menu"></i>
           <span>用户管理</span>
         </template>
         <el-menu-item-group>
-
           <router-link to="/mis/userList" style="text-decoration:none;">
             <el-menu-item index="1-1">用户列表</el-menu-item>
           </router-link>
-
-          <el-menu-item index="1-2">选项2</el-menu-item>
+          <router-link to="/mis/userList" style="text-decoration:none;">
+            <el-menu-item index="1-2">新增用户</el-menu-item>
+          </router-link>
         </el-menu-item-group>
       </el-submenu>
-      <el-submenu index="2" disabled>
+      <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-location"></i>
           <span>门店管理</span>
         </template>
         <el-menu-item-group>
-          <el-menu-item index="2-1">门店列表</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
+          <router-link to="/mis/userList" style="text-decoration:none;">
+            <el-menu-item index="2-1">门店列表</el-menu-item>
+          </router-link>
+          <router-link to="/mis/userList" style="text-decoration:none;">
+            <el-menu-item index="2-2">新增门店</el-menu-item>
+          </router-link>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-goods"></i>
+          <span>商品管理</span>
+        </template>
+        <el-menu-item-group>
+          <router-link to="/mis/goodsList" style="text-decoration:none;">
+            <el-menu-item index="3-1">商品列表</el-menu-item>
+          </router-link>
+          <router-link to="/mis/goodsList" style="text-decoration:none;">
+            <el-menu-item index="3-2">新增商品</el-menu-item>
+          </router-link>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-setting"></i>
+          <span>服务管理</span>
+        </template>
+        <el-menu-item-group>
+          <router-link to="/mis/goodsList" style="text-decoration:none;">
+            <el-menu-item index="4-1">服务列表</el-menu-item>
+          </router-link>
+          <router-link to="/mis/goodsList" style="text-decoration:none;">
+            <el-menu-item index="4-2">新增服务</el-menu-item>
+          </router-link>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="5">
+        <template slot="title">
+          <i class="el-icon-picture"></i>
+          <span>宠物管理</span>
+        </template>
+        <el-menu-item-group>
+          <router-link to="/mis/goodsList" style="text-decoration:none;">
+            <el-menu-item index="5-1">宠物列表</el-menu-item>
+          </router-link>
+          <router-link to="/mis/goodsList" style="text-decoration:none;">
+            <el-menu-item index="5-2">新增宠物</el-menu-item>
+          </router-link>
         </el-menu-item-group>
       </el-submenu>
     </el-menu>
@@ -41,7 +83,7 @@
 
 <script>
 export default {
-  name: "mis",
+  name: "mis"
 };
 </script>
 
