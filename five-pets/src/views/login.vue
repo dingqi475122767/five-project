e7d423efe8588863a3c4fbe5d632891db7c068fb
DYQ\Who <1584251163@qq.com>
2019-02-18 16:12:26 +0800

    登陆注册样式

--- five-pets/src/views/login.vue
+++ five-pets/src/views/login.vue
@@ -1,24 +1,65 @@
 <template>
-  <div>
-    <el-button type="primary" @click="login">登录（点击进入系统）</el-button>
-    <el-button type="primary" @click="reg">注册（点击跳转注册）</el-button>
+  <div class="big">
+    <p>登陆爱宠邦管理系统</p>
+    <div style="margin: 20px;"></div>
+    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="账号">
+        <el-input v-model="formLabelAlign.name" placeholder="请输入你的个人账号"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formLabelAlign.region" placeholder="请输入你的密码"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary" @click="login" class="loginbtn">登陆</el-button>
+    <div style="margin: 20px;"></div>
+    <div>没有账号 ? <span @click="reg"> 申请一个新账号</span> .</div>
+    <div style="margin: 20px;"></div>
   </div>
 </template>
 
 <script>
 export default {
-    name:"login",
-    methods:{
-        reg(){
-            this.$router.history.push('/reg')
-        },
-        login(){
-            this.$router.history.push('/mis')
-            
-        }
+  name: "login",
+  methods: {
+    reg() {
+      this.$router.history.push("/reg");
+    },
+    login() {
+      this.$router.history.push("/mis");
     }
+  },
+  data() {
+    return {
+      labelPosition: "right",
+      formLabelAlign: {
+        name: "",
+        region: "",
+        type: ""
+      }
+    };
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.big{
+    width: 400px;
+    margin: 0 auto;
+    border: 1px solid #f4f4f4;
+    text-align: center;
+    padding-left:20px;
+    padding-right: 25px;
+}
+p{
+    color: #333;
+    font-size: 20px;
+}
+span{
+    color: #0366d6;
+    cursor: pointer;
+}
+.loginbtn{
+    width: 320px;
+    margin-left: 78px;
+}
 </style>
--- five-pets/src/views/register.vue
+++ five-pets/src/views/register.vue
@@ -1,20 +1,69 @@
 <template>
-  <div>
-    <el-button type="primary">提交</el-button>
-    <el-button type="primary" @click="black">返回（点击登录页面）</el-button>
+  <div class="box">
+    <p>创建你的个人账户</p>
+    <div style="margin: 20px;"></div>
+    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="账号">
+        <el-input v-model="formLabelAlign.name" placeholder="创建账户"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formLabelAlign.region" placeholder="创建密码"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formLabelAlign.region" placeholder="确认密码"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary" class="regbtn" @click="confirm">注册爱宠邦账户</el-button>
+    <div style="margin: 20px;"></div>
+    <div @click="black" class="return">返回登陆页面</div>
   </div>
 </template>
 
 <script>
 export default {
-    name:'reg',
-    methods:{
-        black(){
-            this.$router.history.push('/login')
-        }
+  name: "reg",
+  methods: {
+    black() {
+      this.$router.history.push("/login");
+    },
+    confirm() {
+      this.$alert("<p>等待管理人员审核</p>", "提示：", {
+        dangerouslyUseHTMLString: true
+      });
     }
+  },
+  data() {
+    return {
+      labelPosition: "right",
+      formLabelAlign: {
+        name: "",
+        region: "",
+        type: ""
+      }
+    };
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.box {
+  width: 400px;
+  margin: 0 auto;
+  border: 1px solid #f4f4f4;
+  text-align: center;
+  padding-left: 20px;
+  padding-right: 25px;
+}
+p {
+  color: #333;
+  font-size: 20px;
+}
+.return {
+  color: #0366d6;
+  cursor: pointer;
+}
+.regbtn {
+  width: 320px;
+  margin-left: 78px;
+}
 </style>
