4f6a84ae2f1351277c619e6b21fd1b81b452348c
MIR-DIN\Mr.Din <475122767@qq.com>
2019-02-19 15:03:39 +0800

    注册数据发送

--- five-pets/src/store.js
+++ five-pets/src/store.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import shop from './module/shop'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import shop from './module/shop';
+import shopUsers from './module/shopUsers';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    shop
-    
+    shop,
+    shopUsers
   }
 })
