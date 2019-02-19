3880df52ade288348d452dcb88daf57da7e4e744
WIN-E0Q048I039N\Who <253306341@qq.com>
2019-02-19 15:23:12 +0800

    addService

--- five-pets/src/store.js
+++ five-pets/src/store.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import shop from './module/shop'
-
+import services from './module/services'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    shop
-    
+    shop,
+    services
   }
 })
