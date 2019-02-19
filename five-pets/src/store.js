6280c3c787cc943e6892247d23f8cd57a76677ca
月夜小白龙 <594877445@qq.com>
2019-02-19 15:01:56 +0800

    第一次提交goods相关文件

--- five-pets/src/store.js
+++ five-pets/src/store.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import shop from './module/shop'
+import shop from './module/shop';
+import goods from './module/goods'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    shop
-    
+    shop,goods
   }
 })
