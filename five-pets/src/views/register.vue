9b19edb2dd2a159ffb67d957d3d4b0efd009ac68
DYQ\Who <1584251163@qq.com>
2019-02-19 11:29:00 +0800

    注册

--- five-pets/src/views/register.vue
+++ five-pets/src/views/register.vue
@@ -4,13 +4,16 @@
     <div style="margin: 20px;"></div>
     <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
       <el-form-item label="账号">
-        <el-input v-model="formLabelAlign.name" placeholder="创建账户"></el-input>
+        <el-input @blur="phoneNum" v-model="account" placeholder="创建账户"></el-input>
+        <!-- account是账户的意思,phoneNum 是注册手机号失焦 -->
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formLabelAlign.region" placeholder="创建密码"></el-input>
+        <el-input @blur="regpass" v-model="regPassword" placeholder="创建密码" type="password"></el-input>
+        <!-- regPassword 门店用户注册密码,regpass 是注册密码失焦 -->
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formLabelAlign.region" placeholder="确认密码"></el-input>
+        <el-input v-model="affirm" placeholder="确认密码" type="password"></el-input>
+        <!-- affirm门店用户确认密码 -->
       </el-form-item>
     </el-form>
     <el-button type="primary" class="regbtn" @click="confirm">注册爱宠邦账户</el-button>
@@ -27,14 +30,45 @@ export default {
       this.$router.history.push("/login");
     },
     confirm() {
-      this.$alert("<p>等待管理人员审核</p>", "提示：", {
+      //confirm确认注册
+      this.$alert("<p>注册成功,等待管理人员审核</p>", "提示：", {
         dangerouslyUseHTMLString: true
       });
+      const account = this.account; //账户
+      const regPassword = this.regPassword; //注册密码
+      const affirm = this.affirm; //确认密码
+      // this.$store.commit("/shopUsers/","account"),
+      this.account = "";
+      this.regPassword = "";
+      this.affirm = "";
+    },
+    phoneNum() {
+      //手机号正则验证
+      let phone = /^1[3,5,8,7]\d{9}$/;
+      const account = this.account; //账户
+      if (phone.test(account)) {
+        console.log("sssss");
+      } else {
+        this.$alert("<p>手机号错误</p>", "提示：", {
+          dangerouslyUseHTMLString: true
+        });
+        this.account = "";
+      }
+    },
+    regpass() {
+      //密码正则验证
+      let password = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,21}$/;
+      const regPassword = this.regPassword; //注册密码
+      if (password.test(regPassword) == "") {
+      }
     }
   },
   data() {
     return {
       labelPosition: "right",
+      account: "",
+      regPassword: "",
+      affirm: "",
       formLabelAlign: {
         name: "",
         region: "",
