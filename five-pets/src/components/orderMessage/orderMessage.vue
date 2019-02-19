d958346d92cd8384aa6db5cc767e6d7dad98a27d
MIR-DIN\Mr.Din <475122767@qq.com>
2019-02-19 13:23:08 +0800

    添加了代理文件

--- five-pets/src/components/orderMessage/orderMessage.vue
+++ five-pets/src/components/orderMessage/orderMessage.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
-    <el-table :data="tableData" border style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+    <el-table 
+    :data="tableData" 
+    border 
+    stripe
+    style="width: 100%" 
+    height="300"
+    :default-sort="{prop:'date',order:'descending'}"
+    >
+      <el-table-column prop="_id" label="订单号"></el-table-column>
+      <el-table-column prop="shopName" label="商品名"></el-table-column>
+      <el-table-column prop="orderTime" label="订单时间" sortable></el-table-column>
+      <el-table-column prop="address" label="收获地址"></el-table-column>
+      <el-table-column prop="users" label="用户名"></el-table-column>
+      <el-table-column prop="totalPrice" label="订单金额" sortable></el-table-column>
+      <el-table-column
+      fixed="right"
+      label="操作"
+      width="100">
+      <template slot-scope="scope">
+        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+        <el-button type="text" size="small">编辑</el-button>
+      </template>
+    </el-table-column>
     </el-table>
   </div>
 </template>
@@ -11,28 +30,45 @@
 <script>
 export default {
   name: "orderMessage",
+  methods:{
+    handleClick(row){
+      console.log(row)
+    }
+  },
   data() {
     return {
       tableData: [
         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 路"
+          _id: "20160502",
+          users: "王小虎",
+          address: "上海市普陀区金沙江路 1518 路",
+          shopName: "狗粮",
+          orderTime:"201911111",
+          totalPrice:"99",
         },
         {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 路"
+          _id: "20160504",
+          users: "王小虎",
+          address: "上海市普陀区金沙江路 1517 路",
+          shopName: "狗粮",
+          orderTime:"201911111",
+          totalPrice:"99",
         },
         {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 路"
+          _id: "20160501",
+          users: "王小虎",
+          address: "上海市普陀区金沙江路 1519 路",
+          shopName:"狗粮",
+          orderTime:"201911111",
+          totalPrice:"99",
         },
         {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 路"
+          _id: "20160503",
+          users: "王小虎",
+          address: "上海市普陀区金沙江路 1516 路",
+          shopName:"狗粮",
+          orderTime:"201911111",
+          totalPrice:"99",
         }
       ]
     };
