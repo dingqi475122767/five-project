7785347d93b4cee7a508ed561f1a893d23da6405
月夜小白龙 <594877445@qq.com>
2019-02-20 17:42:11 +0800

    。。。

--- my-service/dao/goodsDao.js
+++ my-service/dao/goodsDao.js
@@ -17,12 +17,7 @@ module.exports.getAllByPage = async function({currentPage,eachPage,shopUserID}){
         .find({isDel:false,shopUserID})
         .skip((currentPage - 1) * eachPage)
         .limit(eachPage)
-<<<<<<< HEAD
-
-    return {currentPage,eachPage,totalNum,totalPage,data}
-=======
     return {currentPage,eachPage,totalNum,totalPage,data};
->>>>>>> 0441f0cc51b120b7de3c77138707d85d529e0774
 }
 
 // 删除商品，将商品的isDel属性修改为true，表示已经删除，页面上不再显示
@@ -30,9 +25,5 @@ module.exports.delGoods = async ({_id,isDel}) => await goodsModel.updateOne({_id
 
 // 修改商品信息
 module.exports.updateGoods = async ({_id,goodsName,goodsType,standard,packaging,price})=>{
-<<<<<<< HEAD
-    return await goodsModel.updateOne({_id},{goodsName,goodsType,standard,packaging,price})
-=======
     return await goodsModel.updateOne({_id},{goodsName,goodsType,standard,packaging,price});
->>>>>>> 0441f0cc51b120b7de3c77138707d85d529e0774
 }
