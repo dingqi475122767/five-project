2b78d8fd98bb14286f0215e81c18bc748033f333
liuyou <861357933@qq.com>
2019-02-20 16:51:44 +0800

    小改动

--- five-pets/src/components/pets/updatePet.vue
+++ five-pets/src/components/pets/updatePet.vue
@@ -76,15 +76,32 @@ export default {
     handleUpdatePets(pet) {
       this.$refs[pet].validate(valid => {
         if (valid) {
-          this.updateAsync();
-            this.$router.push("/mis/petList");
+          this.$confirm("此操作将永久修改该文件, 是否继续?", "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning"
+          })
+            .then(() => {
+              this.$message({
+                type: "success",
+                message: "修改成功!"
+              });
+                this.updateAsync();
+              this.$router.push("/mis/petList");
+            })
+            .catch(() => {
+              this.$message({
+                type: "info",
+                message: "已取消修改"
+              });
+            });
         } else {
           return false;
         }
       });
     },
     cancelUpdate(pet) {
-       this.$router.push("/mis/petList");
+      this.$router.push("/mis/petList");
     }
   },
   computed: {
