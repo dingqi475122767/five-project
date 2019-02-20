c3ceef25b6e78599e7dc46c46307f208830121a2
WIN-E0Q048I039N\Who <253306341@qq.com>
2019-02-20 15:21:56 +0800

    store.js保留多方更改

--- five-pets/src/store.js
+++ five-pets/src/store.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-<<<<<<< HEAD
-import services from './module/services';
-import shop from './module/shop';
-import goods from './module/goods';
-=======
 
 import pets from "./module/pets"
 
@@ -12,7 +7,6 @@ import shop from './module/shop'
 import services from './module/services'
 import goods from './module/goods'
 
->>>>>>> 345150c6a08b1139b46c70a96f46698bf34056a0
 import shopUsers from './module/shopUsers';
 import users from './module/users';
 
@@ -20,18 +14,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-<<<<<<< HEAD
-    shop,
-    services,
-    goods,
-    shopUsers
-=======
     pets,
     services,
     shop,
     goods,
     shopUsers,
     users
->>>>>>> 345150c6a08b1139b46c70a96f46698bf34056a0
   }
 })
