76d5af526f04383e3d5298ef3f4c475c533d2699
WIN-E0Q048I039N\Who <253306341@qq.com>
2019-02-22 11:52:49 +0800

    .

--- five-pets/src/module/shopUsers.js
+++ five-pets/src/module/shopUsers.js
@@ -1,9 +1,15 @@
-<<<<<<< HEAD
-import { regAsync, loginAsync, isShopUsersAsync, getUsersByPageAsync, updateShopUsersAsync, isLoginAsync } from '../services/shopUsers'
-=======
-import { regAsync, loginAsync, isShopUsersAsync, getUsersByPageAsync, updateShopUsersAsync, auditShopUsersAsync,getAuditByPage } from '../services/shopUsers'
->>>>>>> dbe0db900220481d24913e3e3010f43dc3f04b93
-import router from '../router';//要用路径跳转就把东西写到要用的页面就可以了
+// import { regAsync, loginAsync, isShopUsersAsync, getUsersByPageAsync, updateShopUsersAsync, isLoginAsync } from '../services/shopUsers'
+import {
+  regAsync,
+  loginAsync,
+  isShopUsersAsync,
+  getUsersByPageAsync,
+  updateShopUsersAsync,
+  isLoginAsync,
+  auditShopUsersAsync,
+  getAuditByPage
+} from '../services/shopUsers'
+import router from '../router'; //要用路径跳转就把东西写到要用的页面就可以了
 
 export default {
   namespaced: true,
@@ -17,11 +23,8 @@ export default {
     totalNum: 0, //总数据
     totalPage: 0, //总页数
     data: [],
-<<<<<<< HEAD
-    updateInfo: {},//要修改的账号数据
-    isLogin:false,//登录状态
-=======
-    updateInfo: {},//储存用户信息
+    updateInfo: {}, //要修改的账号数据
+    isLogin: false, //登录状态
     audit: {
       currentPage: 1, //当前页
       eachPage: 5, //每页显示条数
@@ -29,7 +32,6 @@ export default {
       totalPage: 0, //总页数
       data: [],
     }
->>>>>>> dbe0db900220481d24913e3e3010f43dc3f04b93
   },
   mutations: {
     set(state, payload) {
@@ -56,23 +58,21 @@ export default {
     getShopUsersInfo: (state) => {
       state.updateInfo = JSON.parse(sessionStorage.shopUsersInfo)
     },
-<<<<<<< HEAD
     //请求得到的登录状态
-    setIsLogin(state,payload){
+    setIsLogin(state, payload) {
       state.isLogin = payload
-=======
+    },
     //获取待审核用户
     getAuditByPage: (state, payload) => {
       Object.assign(state.audit, payload)
     },
     //设置待审核用户的当前页
-    setAuditCurPage(state,payload){
+    setAuditCurPage(state, payload) {
       state.audit.currentPage = payload
     },
     //设置待审核用户的每页显示条数
-    setAuditEachPage(state,payload){
+    setAuditEachPage(state, payload) {
       state.audit.eachPage = payload
->>>>>>> dbe0db900220481d24913e3e3010f43dc3f04b93
     }
   },
   actions: {
@@ -81,7 +81,10 @@ export default {
       await regAsync(payload)
     },
     //门店登陆
-    loginAsync: async ({ commit, state }, payload) => {//payload是拿到用户的输入
+    loginAsync: async ({
+      commit,
+      state
+    }, payload) => { //payload是拿到用户的输入
       const isLogin = await loginAsync(payload);
       if (isLogin.data) {
         const value = JSON.stringify(isLogin.data)
@@ -93,14 +96,22 @@ export default {
     },
 
     //验证是否门店账户是否重复
-    isShopUsersAsync: async ({ state, commit }, payload) => {
+    isShopUsersAsync: async ({
+      state,
+      commit
+    }, payload) => {
       let data = await isShopUsersAsync(payload);
-      commit("setIsRepet", data.data)//commit是一个方法
+      commit("setIsRepet", data.data) //commit是一个方法
     },
 
     //查询翻页信息
-    getUsersByPageAsync: async ({ commit, state }) => {
-      const { data } = await getUsersByPageAsync({
+    getUsersByPageAsync: async ({
+      commit,
+      state
+    }) => {
+      const {
+        data
+      } = await getUsersByPageAsync({
         currentPage: state.currentPage,
         eachPage: state.eachPage
       })
@@ -108,31 +119,45 @@ export default {
     },
 
     //修改用户信息
-    updateShopUsersAsync: async ({ dispatch }, payload) => {
+    updateShopUsersAsync: async ({
+      dispatch
+    }, payload) => {
       await updateShopUsersAsync(payload);
       dispatch("getUsersByPageAsync");
     },
 
-<<<<<<< HEAD
     //查看登录状态
-    isLogin: async ({commit}) => {
+    isLogin: async ({
+      commit
+    }) => {
       const state = await isLoginAsync()
       console.log(state)
-      // commit("setIsLogin")
-=======
+    },
+    // commit("setIsLogin")
     //审核用户信息
-    auditShopUsersAsync: async ({ dispatch }, payload) => {
+    auditShopUsersAsync: async ({
+      dispatch
+    }, payload) => {
       await auditShopUsersAsync(payload);
       dispatch("getAuditByPageAsync");
     },
 
     //获取待审核用户
-    getAuditByPageAsync: async ({ commit, state }) => {
-      const { currentPage, eachPage } = state.audit
-      const {data} = await getAuditByPage({currentPage,eachPage})
-      commit("getAuditByPage",data)
->>>>>>> dbe0db900220481d24913e3e3010f43dc3f04b93
+    getAuditByPageAsync: async ({
+      commit,
+      state
+    }) => {
+      const {
+        currentPage,
+        eachPage
+      } = state.audit
+      const {
+        data
+      } = await getAuditByPage({
+        currentPage,
+        eachPage
+      })
+      commit("getAuditByPage", data)
     }
   }
 }
-
